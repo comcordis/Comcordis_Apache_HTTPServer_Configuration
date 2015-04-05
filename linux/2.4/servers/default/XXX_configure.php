ca774c87ae361580727f058a34a3c470021c85f9
vinceMeens <v.w.a.meens@comcordis.com>
2015-04-06 01:46:55 +0200

    Minor fixes

--- linux/2.4/servers/default/XXX_configure.php
+++ linux/2.4/servers/default/XXX_configure.php
@@ -1,8 +1,8 @@
 <?php
 
-include '../../../../../../XXX_ServerConfiguration/development/centOS7/XXX_ServerConfiguration.php';
+include '../../../../../../XXX_ServerConfiguration/operations/centOS7/XXX_ServerConfiguration.php';
 
-$composedContent = XXX_ServerConfiguration::composeConfigurationFileWithIncludes(XXX_ServerConfiguration::$paths['applicationSources'] . '/Comcordis_Apache_HTTPServer_Configuration/development/linux/2.4/servers/default/initialize.conf');
+$composedContent = XXX_ServerConfiguration::composeConfigurationFileWithIncludes(XXX_ServerConfiguration::$paths['applicationSources'] . '/Comcordis_Apache_HTTPServer_Configuration/operations/linux/2.4/servers/default/initialize.conf');
 
 $destinationPath = '/etc/httpd/conf.d/composed.Comcordis_Apache_HTTPServer_Configuration.initialize.conf';
 
